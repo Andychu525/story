e87c8104cc80a002e7cc27299e1a8431b313f403
Andychu525 <525715781@qq.com>
2019-05-22 23:52:50 +0800

    demo

--- client_md/src/main.ts
+++ client_md/src/main.ts
@@ -1,12 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+
+import './style/index.styl'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.config.productionTip = false
+Vue.use(Vuetify)
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
